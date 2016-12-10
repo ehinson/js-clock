716a3e47413e14d4f38b958b0561d4d9725eaba8
Erin Hinson <hinson.erin@gmail.com>
2016-12-10 13:39:40 -0500

    change background color with time

--- app.js
+++ app.js
@@ -13,14 +13,26 @@ function addZero(i) {
     return i;
 };
 
+function toHex(n){
+  if (n < 16) {
+    n = "0" + n.toString(16);
+  }
+  return n.toString(16);
+
+}
+
 function setDate() {
   const now = new Date();
-  const seconds = addZero(now.getSeconds());
-  const minutes = addZero(now.getMinutes());
-  const hours = addZero(now.getHours());
+  var seconds = addZero(now.getSeconds());
+  var minutes = addZero(now.getMinutes());
+  var hours = addZero(now.getHours());
+
+
+
   const secondsDegrees = (seconds * 6 )+ 90;
   const minutesDegrees = (minutes * 6 )+ 90;
   const hoursDegrees = (hours * 30 )+ 90;
+
   if (hours == 00){
     hourDigital.innerHTML = '12:';
   }else {
@@ -33,6 +45,10 @@ function setDate() {
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
   minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+  console.log(toHex(hours));
+
+  document.body.style.backgroundColor = '#' + toHex(hours)+ toHex(minutes)+ toHex(seconds);
 }
 
 setInterval(setDate, 1000);
