ae3b077172e2e0f232f33eaf09f77b5431f0d8f9
Erin Hinson <hinson.erin@gmail.com>
2016-12-10 12:08:15 -0500

    Add Zero and print time

--- app.js
+++ app.js
@@ -1,21 +1,38 @@
 const secondHand = document.querySelector('.seconds');
 const minuteHand = document.querySelector('.minutes');
 const hourHand = document.querySelector('.hours');
+var color = '';
+const secondDigital= document.querySelector('.second-digital');
+const hourDigital = document.querySelector('.hour-digital');
+const minuteDigital = document.querySelector('.minute-digital');
+
+function addZero(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+};
 
 function setDate() {
   const now = new Date();
-  const seconds = now.getSeconds();
-  const minutes = now.getMinutes();
-  const minutes = now.getHours();
+  const seconds = addZero(now.getSeconds());
+  const minutes = addZero(now.getMinutes());
+  const hours = addZero(now.getHours());
   const secondsDegrees = (seconds * 6 )+ 90;
   const minutesDegrees = (minutes * 6 )+ 90;
   const hoursDegrees = (hours * 30 )+ 90;
-  console.log(minutes);
-  console.log(minutesDegrees);
+  if (hours == 00){
+    hourDigital.innerHTML = '12:';
+  }else {
+    secondDigital.innerHTML = seconds ;
+    minuteDigital.innerHTML = minutes + ':' ;
+    hourDigital.innerHTML = hours + ':' ;
+  }
+
 
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
   minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
-  hourHand.style.transform = `rotate(${hoursDegrees }deg)`;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
