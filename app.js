266414d310c63bee603dd16b7830415615493f4d
Erin Hinson <hinson.erin@gmail.com>
2016-12-10 11:44:41 -0500

    Rotate minute, hour, second hands

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
+const secondHand = document.querySelector('.seconds');
+const minuteHand = document.querySelector('.minutes');
+const hourHand = document.querySelector('.hours');
+
 function setDate() {
   const now = new Date();
   const seconds = now.getSeconds();
-  const secondsDegrees = seconds * 6;
-  console.log(seconds);
-  console.log(secondsDegrees);
+  const minutes = now.getMinutes();
+  const minutes = now.getHours();
+  const secondsDegrees = (seconds * 6 )+ 90;
+  const minutesDegrees = (minutes * 6 )+ 90;
+  const hoursDegrees = (hours * 30 )+ 90;
+  console.log(minutes);
+  console.log(minutesDegrees);
+
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+  hourHand.style.transform = `rotate(${hoursDegrees }deg)`;
 }
 
 setInterval(setDate, 1000);
