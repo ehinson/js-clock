f07ac1fe5ad46af3fada95b030b5952c055eac58
Erin Hinson <erinh@xceleration.com>
2017-07-20 14:47:12 -0400

    Change fonts and set timeout

--- app.js
+++ app.js
@@ -2,50 +2,52 @@ const secondHand = document.querySelector('.seconds');
 const minuteHand = document.querySelector('.minutes');
 const hourHand = document.querySelector('.hours');
 var color = '';
-const secondDigital= document.querySelector('.second-digital');
+const secondDigital = document.querySelector('.second-digital');
 const hourDigital = document.querySelector('.hour-digital');
 const minuteDigital = document.querySelector('.minute-digital');
 
 function addZero(i) {
-    if (i < 10) {
-        i = "0" + i;
-    }
-    return i;
-};
-
-function toHex(n){
-  if (n < 16) {
-    n = "0" + n.toString(16);
+  if (i < 10) {
+    i = '0' + i;
   }
-  return n.toString(16);
+  return i;
+}
 
+function toHex(n) {
+  return n.toString(16);
 }
 
 function setDate() {
   const now = new Date();
 
   const seconds = addZero(now.getSeconds());
-  const secondsDegrees = (seconds * 6 )+ 90;
+  const secondsDegrees = seconds * 6 + 90;
 
   const minutes = addZero(now.getMinutes());
-  const minutesDegrees = (minutes * 6 )+ 90;
+  const minutesDegrees = minutes * 6 + 90;
 
   const hours = addZero(now.getHours());
-  const hoursDegrees = (hours * 30  )+ 90;
+  const hoursDegrees = hours * 30 + 90;
 
-  if (hours == 00){
+  if (hours == 00) {
     hourDigital.innerHTML = '12:';
-  }else {
-    secondDigital.innerHTML = seconds ;
-    minuteDigital.innerHTML = minutes + ':' ;
-    hourDigital.innerHTML = hours + ':' ;
+  } else {
+    secondDigital.innerHTML = seconds;
+    minuteDigital.innerHTML = minutes + ':';
+    hourDigital.innerHTML = hours + ':';
   }
 
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
   minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 
-  document.body.style.backgroundColor = '#' + toHex(hours)+ toHex(minutes)+ toHex(seconds);
+  document.body.style.backgroundColor = '#' + toHex(hours * 24) + toHex(minutes) + toHex(seconds);
+}
+function setTime() {
+  return 5000;
 }
 
 setInterval(setDate, 1000);
+setTimeout(function() {
+  alert('Hello');
+}, setTime());
